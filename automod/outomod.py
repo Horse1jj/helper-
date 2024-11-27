8d1dd954ff083bea3dedd9be9fe1e05bad635c07
Horse1jj <foste1jj.seth@gmail.com>
2024-11-27 16:08:10 -0500

    Update outomod.py

--- automod/outomod.py
+++ automod/outomod.py
@@ -1,1 +1,15 @@
+from discord.ext import commands
+
+class AutoMod(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "badword" in message.content.lower():
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}, watch your language!")
+
+def setup(bot):
+    bot.add_cog(AutoMod(bot))
 
