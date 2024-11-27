70d51e0cb3d3a59e24aaacbdad5a0042d496cfd4
Horse1jj <foste1jj.seth@gmail.com>
2024-11-27 16:09:23 -0500

    Update logging.py

--- logging/logging.py
+++ logging/logging.py
@@ -1,1 +1,17 @@
+from discord.ext import commands
+
+class Logging(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f"{member} has joined the server.")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f"{member} has left the server.")
+
+def setup(bot):
+    bot.add_cog(Logging(bot))
 
